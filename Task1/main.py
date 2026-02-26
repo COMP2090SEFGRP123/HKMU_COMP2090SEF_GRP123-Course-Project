dc844ef09827a16aaabeff7612f5fa33df49311d
COMP2090SEFGRP123 <shunfeizhang250@gmail.com>
2026-02-26 23:48:10 +0800

    temp1-02262347

--- Task1/main.py
+++ Task1/main.py
@@ -1,1 +1,10 @@
+from tkinter import *
 
+root = Tk()
+title = Label(root, text='Libary Manage System')
+title.pack()
+action = Label(root, text='Enter 1 to borrow, enter 0 to return')
+action.pack()
+user_act = Entry(root)
+user_act.pack()
+root.mainloop()
