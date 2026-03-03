cf0678e1affb5fcfcdfd21b9595348b046f73fba
COMP2090SEFGRP123 <shunfeizhang250@gmail.com>
2026-03-03 23:23:22 +0800

    Update models.py

--- Task1/models.py
+++ Task1/models.py
@@ -27,19 +27,6 @@ class Book:
             status = "Available"
         return self._title + " by " + self._author + " (ISBN: " + self._isbn + ") - " + status
 
-    def borrow_book(self, book):
-        if book._borrowed:
-            return False, "Book already borrowed."
-        book.borrow()
-        return True, "Book borrowed successfully."
-
-
-    def return_book(self, book):
-        if not book._borrowed:
-            return False, "Book was not borrowed."
-        book.return_book()
-        return True, "Book returned successfully."
-
     @property
     def title(self):
         return self._title
@@ -53,5 +40,44 @@ class Book:
         return self._isbn
 
     @property
+    def year(self):
+        return self._year
+
+    @property
     def is_borrowed(self):
         return self._borrowed
+
+
+class User:
+    def __init__(self, name):
+        self._name = name
+
+    def get_role(self):
+        return "User"
+
+    def borrow_book(self, book):
+        if book.is_borrowed:
+            return False, "Book already borrowed."
+        book.borrow()
+        return True, "Book borrowed successfully."
+
+    def return_book(self, book):
+        if not book.is_borrowed:
+            return False, "Book was not borrowed."
+        book.return_book()
+        return True, "Book returned successfully."
+
+
+class Admin(User):
+    def get_role(self):
+        return "Admin"
+
+    def add_book_to_library(self, library, book):
+        library.add_book(book)
+        return "Book added to library."
+
+    def remove_book_from_library(self, library, book):
+        if book in library.books:
+            library.books.remove(book)
+            return "Book removed from library."
+        return "Book not found."
