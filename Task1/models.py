d01f391b6bbc8b09e16b4abc6abaee650be69d07
COMP2090SEFGRP123 <shunfeizhang250@gmail.com>
2026-03-03 23:12:06 +0800

    Update models.py

--- Task1/models.py
+++ Task1/models.py
@@ -26,3 +26,19 @@ class Book:
         else:
             status = "Available"
         return self._title + " by " + self._author + " (ISBN: " + self._isbn + ") - " + status
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def isbn(self):
+        return self._isbn
+
+    @property
+    def is_borrowed(self):
+        return self._borrowed
